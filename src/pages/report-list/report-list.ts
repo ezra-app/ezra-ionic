fc75afb41eb8ee166b50c9b2084b243ee2e71596
Humberto Machado <betolinck@gmail.com>
2017-01-29 21:56:30 -0300

    Adicionado mensagem de confirmação no click de remover

--- src/pages/report-list/report-list.ts
+++ src/pages/report-list/report-list.ts
@@ -2,7 +2,7 @@ import { EditionPage } from '../edition/edition';
 import { ReportService } from '../../providers/report-service';
 import { ReportModel } from '../../models/report-model';
 import { Component } from '@angular/core';
-import { ModalController, NavController, NavParams, Events } from 'ionic-angular';
+import { AlertController, Events, ModalController, NavController, NavParams } from 'ionic-angular';
 
 /*
   Generated class for the ReportList page.
@@ -17,9 +17,11 @@ import { ModalController, NavController, NavParams, Events } from 'ionic-angular
 export class ReportListPage {
 
   reports: ReportModel[] = new Array<ReportModel>();
+  muliSelectEnabled: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    reportService: ReportService, public modalCtrl: ModalController, public events: Events) {
+    reportService: ReportService, public modalCtrl: ModalController, public events: Events,
+    public alertCtrl: AlertController) {
     reportService.loadAllReports().then(result => {
       this.reports = result;
     });
@@ -43,6 +45,32 @@ export class ReportListPage {
   onAddClick(): void {
     let modal = this.modalCtrl.create(EditionPage);
     modal.present();
+  }
+
+  onRemoveClick(report: ReportModel): void {
+    this.showConfirm(report);
+  }
+
+  showConfirm(report: ReportModel): void {
+    let confirm = this.alertCtrl.create({
+      title: 'Remover?',
+      message: 'Tem certeza que deseja remover?',
+      buttons: [
+        {
+          text: 'Sim',
+          handler: () => {
+            console.log('Sim');
+          }
+        },
+        {
+          text: 'Nao',
+          handler: () => {
+            console.log('Não');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
